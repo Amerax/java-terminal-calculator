c12aace8162845dd35674060eb45391dfc745e85
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-06-17 17:17:58 -0400

    Fix formatting and improve switch case structure

--- main.java
+++ main.java
@@ -20,12 +20,10 @@ public class Main{
                     """);
             input = sc.nextInt();
             
-            
-            
             switch(input){
                 case 0:
                     running = false;
-                    System.out.println("Program has been terminated.")
+                    System.out.println("Program has been terminated.");
                     break;
 
                 case 1:
@@ -33,27 +31,30 @@ public class Main{
                     break;
 
                 case 2:
-
+                    System.out.println(subtract());
                     break;
 
                 case 3:
-
+                    System.out.println(multiply());
                     break;
 
                 case 4:
-
+                    System.out.println(divide());
                     break;
 
                 case 5:
-
+                    System.out.println(sqrt());
                     break;
 
                 case 6:
+                    System.out.println("Enter number (nonnegative integer): ");
+                    int x = sc.nextInt();
 
+                    System.out.println(factorial(x));
                     break;
 
                 case 7:
-
+                    System.out.println(exp());
                     break;
 
                 case 8:
@@ -77,6 +78,61 @@ public class Main{
             double y = sc.nextDouble();
 
             return x+y;
+    }
+
+    public static double subtract(){
+            System.out.println("Enter first number: ");
+            double x = sc.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double y = sc.nextDouble();
+
+            return x-y;
+    }
+
+    public static double multiply(){
+            System.out.println("Enter first number: ");
+            double x = sc.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double y = sc.nextDouble();
+
+            return x*y;
+    }
+
+    public static double divide(){
+            System.out.println("Enter first number: ");
+            double x = sc.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double y = sc.nextDouble();
+
+            return x/y;
+    }
+
+    public static double exp(){
+            System.out.println("Enter base number: ");
+            double x = sc.nextDouble();
+
+            System.out.println("Enter exponent number: ");
+            double y = sc.nextDouble();
+
+            return Math.pow(x, y);
+    }
+
+    public static double sqrt(){
+            System.out.println("Enter number: ");
+            double x = sc.nextDouble();
+
+            return Math.sqrt(x);
+    }
+
+    public static double factorial(int x){
+            if(x <= 1){
+                return 1;
+            }
+
+            return x * factorial(x - 1);
     }
 
     public static double absVal(){
